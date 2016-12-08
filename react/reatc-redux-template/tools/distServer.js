ba96c9dd806171e3caefce66f172c30a952e3b30
Sireesh P <Sireesh P>
2016-12-08 09:50:26 +0000

    react

---
+++ react/reatc-redux-template/tools/distServer.js
@@ -0,0 +1,24 @@
+// This file configures a web server for testing the production build
+// on your local machine.
+
+import browserSync from 'browser-sync';
+import historyApiFallback from 'connect-history-api-fallback';
+import compression from 'compression';
+
+// Run Browsersync
+browserSync({
+  port: 3000,
+  ui: {
+    port: 3001
+  },
+  server: {
+    baseDir: 'dist',
+     middleware: [compression()]
+  },
+
+  files: [
+    'src/*.html'
+  ],
+
+  middleware: [historyApiFallback()]
+});
