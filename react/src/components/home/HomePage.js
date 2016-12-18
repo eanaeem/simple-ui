677887eb1bda32ef7f81be3d3d3b442c12d52e94
Sireesh P <Sireesh P>
2016-12-18 19:18:10 +0000

    fix

--- react/src/components/home/HomePage.js
+++ react/src/components/home/HomePage.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Container from '../../sui/ContainerComponent';
-import Modal from '../../sui/ModalComponent';
-
 
 class HomePage extends React.Component {
     render() {
-        let showModal = true;
         return (
-            <Container headerText="Home">
-                <Modal header='test' showModal={showModal}>
-                    Test 123
-                    <button className="sui-btn sui-green sui-ripple">  Test Btn</button>
-                </Modal>
+            <Container>
+                <p>
+                    <input id="Name" className="sui-input-float" />
+                    <label htmlFor="Name" className="sui-label-float"> Name </label>
+
+                </p>
+                <p>
+                    <select className="sui-select-float"> <option></option><option> Test 2 </option> </select>
+                    <label className="sui-label-float"> Name </label>
+
+                </p>
+
             </Container>
         );
     }
